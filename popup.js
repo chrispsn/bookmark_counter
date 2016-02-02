f30b7357023ea56afd0b70ca977e9a5921af2d11
Chris Pearson <chrispsn@gmail.com>
2016-02-02 23:20:33 +1100

    Formatting cleanup

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
 // Docs are here: https://developer.chrome.com/extensions/bookmarks
 
 function count_children(nodes) {
-	var count = 0;
-	nodes.forEach(node => {
-		if ("children" in node) {
-			count += count_children(node.children)
-		} else {
-			count++;
-		}
-	});
-	return count;
+    var count = 0;
+    nodes.forEach(node => {
+        if ("children" in node) {
+            count += count_children(node.children)
+        } else {
+            count++;
+        }
+    });
+    return count;
 }
 
 function write_count(bookmarkTreeNodes) {
-	var total = count_children(bookmarkTreeNodes);
-	document.getElementById("node_count").innerText = total;
+    var total = count_children(bookmarkTreeNodes);
+    document.getElementById("node_count").innerText = total;
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-	// getTree passes on an array of folder nodes
-	chrome.bookmarks.getTree(write_count);
+    // getTree passes on an array of folder nodes
+    chrome.bookmarks.getTree(write_count);
 });
--- popup_func.js
+++ popup_func.js
@@ -5,29 +5,29 @@
 // Calculate the answer...
 
 function count_bookmarks(nodes) {
-	var folders = nodes.filter(has_children)
-	var count_below_this_folder = folders.map(n => n.children)
-								.map(count_bookmarks)
-								.reduce(sum, 0);
-	return count_below_this_folder + (nodes.length - folders.length);
+    var folders = nodes.filter(has_children)
+    var count_below_this_folder = folders.map(n => n.children)
+                                .map(count_bookmarks)
+                                .reduce(sum, 0);
+    return count_below_this_folder + (nodes.length - folders.length);
 }
 
 function has_children(node) {
-	return ("children" in node);
+    return ("children" in node);
 }
 
 function sum(a, b) {
-	return a + b;
+    return a + b;
 }
 
 // Get the answer to the user...
 
 function write_count(nodes) {
-	var count = count_bookmarks(nodes);
-	document.getElementById("node_count").innerText = count;
+    var count = count_bookmarks(nodes);
+    document.getElementById("node_count").innerText = count;
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-	// getTree passes on an array of folders
-	chrome.bookmarks.getTree(write_count);
+    // getTree passes on an array of folders
+    chrome.bookmarks.getTree(write_count);
 });
