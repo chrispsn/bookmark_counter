8ea020de7543859871cca3eb2eeedf092f478f39
Chris Pearson <chrispsn@gmail.com>
2016-02-03 20:50:30 +1100

    List folders containing more than 10 bookmarks

--- popup.js
+++ popup.js
@@ -5,11 +5,30 @@ function count_bookmarks(nodes) {
     nodes.forEach(node => {
         if ('children' in node) {
             count += count_bookmarks(node.children);
+            check_and_list_fatty(node, 10);
         } else {
             count++;
         }
     });
     return count;
+}
+
+function get_leaves(folder) {
+    return folder.children.filter(n => !('children' in n));
+}
+
+function check_and_list_fatty(folder, max_leaves) {
+    var leaves_count = get_leaves(folder).length;
+    if (leaves_count > max_leaves) {
+        write_fatty(folder.title, leaves_count);
+    }
+}
+
+function write_fatty(title, count) {
+    var text = document.createTextNode(title + ': ' + count);
+    var li = document.createElement('LI');
+    li.appendChild(text);
+    document.getElementById('fatty_list').appendChild(li);
 }
 
 function write_count(nodes) {
